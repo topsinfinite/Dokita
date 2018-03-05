11b214f65044e5d0da8231dfbfc7d89a03f3d5ec
Temitope Fatayo <topsinfinite@yahoo.com>
2018-03-04 21:30:38 -0400

    'updated'

---
+++ src/providers/mock-test-centers.ts
@@ -0,0 +1,19 @@
+let testcenters: Array<any> = [
+    {
+        id: 1,
+        name: "Tokun Ajayi",
+        title: "Primary care doctor",
+        phone: "617-244-3672",
+        mobilePhone: "617-244-3672",
+        email: "toks@ionichealth.com",
+        background:"Tokunbo Ajayi, M.D., practices family medicine at Johns Hopkins Community Physicians Howard County. She received her medical degree from SUNY at Buffalo School of Medicine, and also completed her residency in family medicine at University of Maryland Medical Center",
+        education:{
+                degree:"MD, SUNY at Buffalo School of Medicine (2005)",
+                residences:"University of Maryland Medical Center / Family Medicine (2007)",
+                board_certifications:"American Board of Family Medicine / Family Medicine (2009)"
+            },
+        affliation:["Johns Hopkins Hospital","University of Maryland"],   
+        insurance:["EHP","ATHENA","BlueCross"],
+        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/caroline_kingsley.jpg"
+    }
+]
